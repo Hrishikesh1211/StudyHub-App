50c1316151bf263f591b4add5613eeb6f6ffcae9
Labadmin <jle5540@psu.edu>
2024-11-11 10:04:56 -0500

    added firebase

--- App.tsx
+++ App.tsx
@@ -67,8 +67,13 @@ function App(): React.JSX.Element {
   };
 
   const Stack = createNativeStackNavigator();
-
   return (
+      const Stack = createNativeStackNavigator({
+           screens: {
+             Home: BlackThemeHomePage,
+             Profile: BlackThemeProfile
+           },
+         });
       <NavigationContainer>
             <Stack.Navigator>
               <Stack.Screen name="Home" component={BlackThemeHomePage} />
