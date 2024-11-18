8b6fdfd1037a23fe4f4167bc2b199048afafc8df
IanBurdPSU <igb5019@psu.edu>
2024-11-18 14:41:17 -0500

    Implemented Log In screen

--- android/app/src/main/java/com/studyhub/ui/screens/StartPage.tsx
+++ android/app/src/main/java/com/studyhub/ui/screens/StartPage.tsx
@@ -1,9 +1,66 @@
 import * as React from "react";
-import {Text, StyleSheet, View, Pressable, Image} from "react-native";
+import {Text, StyleSheet, View, Pressable, Image, TextInput, Alert} from "react-native";
+import { useNavigation, } from '@react-navigation/native';
+const googleIcon = require("../../../../../res/icons-mdpi/white_google.png");
+const facebookIcon = require("../../../../../res/icons-mdpi/white_facebook.png");
+const xIcon = require("../../../../../res/icons-mdpi/white_x.png");
+// const appleIcon = require("../../../../../res/icons-mdpi/white_apple.png");
+const atIcon = require("../../../../../res/icons-mdpi/green_at_sign.png");
+const lockIcon = require("../../../../../res/icons-mdpi/green_lock.png");
 
 const BlackThemeStartPage = () => {
+    const nav = useNavigation();
+
   	return (
-        <OldPage/>
+        <View style={newStyles.background}>
+            <Text style={newStyles.titleText}>StudyHub</Text>
+
+            <View style={[newStyles.credentialContainer, newStyles.usernameContainer]}>
+                <Image style={newStyles.credentialIcon} resizeMode="cover" source={atIcon} />
+                <TextInput style={newStyles.credentialText} placeholder="Username"></TextInput>
+            </View>
+            <View style={[newStyles.credentialContainer, newStyles.passwordContainer]}>
+                <Image style={newStyles.credentialIcon} resizeMode="cover" source={lockIcon} />
+                <TextInput style={newStyles.credentialText} placeholder="Password"></TextInput>
+            </View>
+
+            <Text style={newStyles.socialText}>Log In With Social Media</Text>
+
+            <View>
+                <Pressable style={[newStyles.socialButton, newStyles.facebookButton]} onPress={() => {
+                    Alert.alert("facebook");
+                }}>
+                    <Image style={newStyles.socialIcon} resizeMode="cover" source={facebookIcon} />
+                </Pressable>
+                <Pressable style={[newStyles.socialButton, newStyles.xButton]}  onPress={() => {
+                    Alert.alert("x");
+                }}>
+                    <Image style={newStyles.socialIcon} resizeMode="cover" source={xIcon} />
+                </Pressable>
+                <Pressable style={[newStyles.socialButton, newStyles.appleButton]} onPress={() => {
+                    Alert.alert("apple");
+                }}>
+                    <Image style={newStyles.socialIcon} resizeMode="cover" source={googleIcon} />
+                </Pressable>
+                <Pressable style={[newStyles.socialButton, newStyles.googleButton]} onPress={() => {
+                    Alert.alert("google");
+                }}>
+                    <Image style={newStyles.socialIcon} resizeMode="cover" source={googleIcon} />
+                </Pressable>
+            </View>
+
+            <Pressable style={[newStyles.signUpContainer, newStyles.confirmButtonContainer]} onPress={()=>{
+                nav.navigate("Home");
+            }}>
+                <Text style={newStyles.buttonText}>Sign Up</Text>
+            </Pressable>
+
+            <Pressable style={[newStyles.signInContainer, newStyles.confirmButtonContainer]} onPress={()=>{
+                nav.navigate("Home");
+            }}>
+                <Text style={newStyles.buttonText}>I Have an Account</Text>
+            </Pressable>
+        </View>
     )
 
 };
@@ -11,11 +68,144 @@ const BlackThemeStartPage = () => {
 const newStyles = StyleSheet.create({
     background: {
         backgroundColor: "#000000cc",
-//         flex: 1,
-        width: "100%",
-        height: 932,
+        flex: 1,
+//         width: "100%",
+//         height: 932,
+//         overflow: "hidden",
+    },
+    confirmButtonContainer: {
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+    	shadowOffset: {
+            width: 0,
+            height: 4
+    	},
+    	shadowRadius: 4,
+    	elevation: 4,
+    	shadowOpacity: 1,
+    	height: 58,
+        width: 230,
+        borderRadius: 40,
+        position: "absolute",
         overflow: "hidden",
     },
+    signUpContainer: {
+        top: 610,
+    	backgroundColor: "#0aa55a",
+        left: 104,
+    },
+    signInContainer: {
+        top: 700,
+        top: 700,
+    	backgroundColor: "#707070",
+    	left: 104,
+    },
+    buttonText: {
+        top: 18,
+    	left: 28,
+    	fontFamily: "Inter-Bold",
+    	width: 173,
+    	height: 21,
+    	fontSize: 15,
+    	textAlign: "center",
+    	color: "#fff",
+    	fontWeight: "700",
+    	lineHeight: 22,
+    	letterSpacing: 0,
+    	position: "absolute"
+    },
+    credentialContainer: {
+    	borderRadius: 11,
+    	backgroundColor: "#707070",
+    	height: 48,
+    	width: 316,
+    	left: 57,
+    	position: "absolute",
+    	overflow: "hidden"
+    },
+    usernameContainer: {
+        top: 315
+    },
+    passwordContainer: {
+        top: 400
+    },
+    credentialText: {
+//         height: 23,
+        top: 5,
+        left: 45,
+    	textAlign: "left",
+    	fontFamily: "Inter-Regular",
+    	color: "#fff",
+//     	letterSpacing: 0,
+    	fontSize: 15,
+    	position: "absolute"
+    },
+    credentialIcon: {
+        top: 13,
+        left: 10,
+        position: "absolute"
+    },
+    socialText: {
+        top: 480,
+    	width: 255,
+    	left: 89,
+   		fontFamily: "Inter-Regular",
+   		height: 21,
+   		textAlign: "center",
+   		color: "#fff",
+//    		lineHeight: 22,
+   		letterSpacing: 0,
+   		fontSize: 15,
+   		position: "relative"
+    },
+    socialButton: {
+        height: 56,
+    	width: 56,
+    	backgroundColor: "#000",
+    	borderRadius: 20,
+    	top: 500,
+    	position: "absolute"
+    },
+    facebookButton: {
+        left: 80
+    },
+    xButton: {
+        left: 150
+    },
+    appleButton: {
+        left: 220
+    },
+    googleButton: {
+        left: 290
+    },
+    socialIcon: {
+    	height: 40,
+   		width: 40,
+   		left: 7,
+   		top: 7,
+   		position: "relative"
+    },
+    titleText: {
+        top: 100,
+    	left: 85,
+    	fontSize: 40,
+   		fontFamily: "InstrumentSans-Bold",
+   		display: "flex",
+   		alignItems: "center",
+   		justifyContent: "center",
+   		width: 260,
+   		height: 186,
+   		textShadowColor: "#00000040",
+   		textShadowOffset: {
+            width: 16,
+     		height: 20
+   		},
+        textShadowRadius: 1,
+    	textAlign: "center",
+   		color: "#fff",
+   		fontWeight: "700",
+   		letterSpacing: 0,
+   		position: "absolute"
+    }
 });
 
 const OldPage = () => {
