4ae983efb011f2acbca8c7821bd7ed59a38870ae
IanBurdPSU <igb5019@psu.edu>
2024-11-24 21:13:31 -0500

    Revert "Upgraded menu bar"
    This reverts commit cdcda1938b2a46f9c1715c9de53077ba5cf4c6e1.
    
    testing changes

--- android/app/src/main/java/com/studyhub/ui/components/MenuBar.tsx
+++ android/app/src/main/java/com/studyhub/ui/components/MenuBar.tsx
@@ -1,60 +1,77 @@
 import * as React from "react";
 import {Image, StyleSheet, Text, Pressable, View, FlatList, ImageBackground} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-
-const homeIcon = require("../../../../../res/icons-mdpi/white_home.png");
-const gameIcon = require("../../../../../res/icons-mdpi/white_box.png");
-const addIcon = require("../../../../../res/icons-mdpi/green_add_button.png");
-const chatIcon = require("../../../../../res/icons-mdpi/white_chat_bubble.png");
-const profileIcon = require("../../../../../res/icons-mdpi/white_person.png");
+import {useNavigation} from "@react-navigation/native";
+const homeIcon = require("../../../../../res/icons-mdpi/white_home.png")
+const gameIcon = require("../../../../../res/icons-mdpi/white_box.png")
+const addIcon = require("../../../../../res/icons-mdpi/green_add_button.png")
+const chatIcon = require("../../../../../res/icons-mdpi/white_chat_bubble.png")
+const profileIcon = require("../../../../../res/icons-mdpi/white_person.png")
 
 export const MenuBar = () => {
     const nav = useNavigation();
 
     return (
         <View style={styles.container}>
-            <Pressable style={styles.iconContainer} onPress={() => nav.navigate("Home")} accessibilityLabel="Home">
-                <Image source={homeIcon} style={styles.icon} />
-                <Text style={styles.iconText}>Home</Text>
+            <Pressable style={styles.homeIcon} onPress={() => {
+                nav.navigate("Home")
+            }}>
+                <Image source={homeIcon}/>
+                <Text>Home</Text>
             </Pressable>
-            <Pressable style={styles.iconContainer} onPress={() => nav.navigate("Games")} accessibilityLabel="Games">
-                <Image source={gameIcon} style={styles.icon} />
-                <Text style={styles.iconText}>Games</Text>
+            <Pressable style={styles.gameIcon}>
+                <Image source={gameIcon}/>
+                <Text>Games</Text>
             </Pressable>
-            <Pressable style={styles.iconContainer} onPress={() => nav.navigate("CreateGroup")} accessibilityLabel="Create Group">
-                <Image source={addIcon} style={styles.icon} />
+            <Pressable style={styles.groupIcon} onPress={() => {
+                nav.navigate("CreateGroup")
+            }}>
+                <Image source={addIcon}/>
             </Pressable>
-            <Pressable style={styles.iconContainer} onPress={() => nav.navigate("Chats")} accessibilityLabel="Chats">
-                <Image source={chatIcon} style={styles.icon} />
-                <Text style={styles.iconText}>Chats</Text>
+            <Pressable style={styles.chatIcon} onPress={() => {
+                nav.navigate("Chats")
+            }}>
+                <Image source={chatIcon}/>
+                <Text>Chats</Text>
             </Pressable>
-            <Pressable style={styles.iconContainer} onPress={() => nav.navigate("Profile")} accessibilityLabel="Profile">
-                <Image source={profileIcon} style={styles.icon} />
-                <Text style={styles.iconText}>Profile</Text>
+            <Pressable style={styles.profileIcon} onPress={() => {
+                nav.navigate("Profile")
+            }}>
+                <Image source={profileIcon}/>
+                <Text>Profile</Text>
             </Pressable>
         </View>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#282828",
+     container: {
+//        flex: 1,
+
+    	backgroundColor: "#282828",
         flexDirection: "row",
-        justifyContent: "space-around",
-
+        backgroundColor: "#282828",
         alignItems: "center",
-        paddingVertical: 10,
+//         justifyContent: "space-between"
+     },
+     ImageBackground: {
+//        flex: 1,
+       resizeMode: "cover",
+       width: "100%",
+       alignItems: "center",
+     },
+    homeIcon: {
+        left: 30
     },
-    iconContainer: {
-        alignItems: "center",
+    gameIcon: {
+        left: 60
     },
-    icon: {
-        width: 24,
-        height: 24,
+    groupIcon: {
+        left: 90
     },
-    iconText: {
-        color: "#fff",
-        fontSize: 12,
+    chatIcon: {
+        left: 120
+    },
+    profileIcon: {
+        left: 150
     }
-
 });
